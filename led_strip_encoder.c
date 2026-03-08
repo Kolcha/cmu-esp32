81fb9aa7ed2f42e7adb5988153ef9b2e88bb6668
Nick Korotysh <nick.korotysh@gmail.com>
2026-03-08 17:23:34 +0100

    tune rmt timings to make them more universal

--- led_strip_encoder.c
+++ led_strip_encoder.c
@@ -87,15 +87,15 @@ esp_err_t rmt_new_led_strip_encoder(const led_strip_encoder_config_t *config, rm
     rmt_bytes_encoder_config_t bytes_encoder_config = {
         .bit0 = {
             .level0 = 1,
-            .duration0 = 0.40 * config->resolution / 1000000, // T0H=0.40us
+            .duration0 = 0.30 * config->resolution / 1000000, // T0H=0.30us
             .level1 = 0,
-            .duration1 = 0.85 * config->resolution / 1000000, // T0L=0.85us
+            .duration1 = 0.90 * config->resolution / 1000000, // T0L=0.90us
         },
         .bit1 = {
             .level0 = 1,
-            .duration0 = 0.80 * config->resolution / 1000000, // T1H=0.80us
+            .duration0 = 0.90 * config->resolution / 1000000, // T1H=0.90us
             .level1 = 0,
-            .duration1 = 0.45 * config->resolution / 1000000, // T1L=0.45us
+            .duration1 = 0.35 * config->resolution / 1000000, // T1L=0.35us
         },
         .flags.msb_first = 1 // WS2812 transfer bit order: G7...G0R7...R0B7...B0
     };
@@ -103,7 +103,7 @@ esp_err_t rmt_new_led_strip_encoder(const led_strip_encoder_config_t *config, rm
     rmt_copy_encoder_config_t copy_encoder_config = {};
     ESP_GOTO_ON_ERROR(rmt_new_copy_encoder(&copy_encoder_config, &led_encoder->copy_encoder), err, TAG, "create copy encoder failed");
 
-    uint32_t reset_ticks = config->resolution / 1000000 * 60 / 2; // reset code duration defaults to 60us
+    uint32_t reset_ticks = config->resolution / 1000000 * 300 / 2; // reset code duration defaults to 300us
     led_encoder->reset_code = (rmt_symbol_word_t) {
         .level0 = 0,
         .duration0 = reset_ticks,
